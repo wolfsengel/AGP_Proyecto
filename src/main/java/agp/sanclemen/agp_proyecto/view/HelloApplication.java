a6d7311d649f9cc11fe1840ece737e2bcfdab511
a22angelgp <a22angelgp@iessanclemente.net>
2024-02-28 23:38:01 +0100

    (*) Fixing Overall ++ ViewImplementation Innit

--- src/main/java/agp/sanclemen/agp_proyecto/view/HelloApplication.java
+++ src/main/java/agp/sanclemen/agp_proyecto/view/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("app-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Carritos de la compra");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/agp/sanclemen/agp_proyecto/app-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Ángemazon");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/agp/sanclemen/agp_proyecto/view/HelloController.java
+++ src/main/java/agp/sanclemen/agp_proyecto/view/HelloController.java
@@ -1,14 +1,6 @@
 package agp.sanclemen.agp_proyecto.view;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.*;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
